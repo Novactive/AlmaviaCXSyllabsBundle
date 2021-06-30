9c27a9dbf108928804ff257fc24dd51ca1320b02
Florian ALEXANDRE <f.alexandre@novactive.com>
2021-06-30 09:56:29 +0200

    first commit

---
+++ src/bundles/APIBundle/SyllabsAPIBundle.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * AlmaviaCXSyllabsBundle.
+ *
+ * @package   AlmaviaCXSyllabsBundle
+ *
+ * @author    florian
+ * @copyright 2018 Novactive
+ * @license   https://github.com/Novactive/NovaHtmlIntegrationBundle/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace AlmaviaCX\Bundle\Syllabs\APIBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SyllabsAPIBundle extends Bundle
+{
+
+}
